1c76ccf844f3a77a8a98c2e16902a8b67b3bcaa7
Jose D. Romero <josedanilromero@gmail.com>
2024-05-05 10:36:39 -0500

    Commit 5: implementing Action Listener interface, added actionPerformed method.

--- JavaCalculator.java
+++ JavaCalculator.java
@@ -2,8 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class JavaCalculator {
+public class JavaCalculator implements ActionListener {
+    JavaCalculator() {
+
+    }
     public static void main (String[] args) {
 
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
     }
 }
